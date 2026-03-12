eb5fb5b8d78f7a04ec5bd9642303f41ba1bc2e99
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2026-03-12 17:18:51 -0300

    2.1.8

--- include/log_format.h
+++ include/log_format.h
@@ -4,36 +4,52 @@
 
 struct ColoredText
 {
-    const char* color;
+    const char *color;
     std::string text;
 };
 
-inline ColoredText magenta(const std::string& s)
+inline ColoredText magenta(const std::string &s)
 {
     return {"\x1B[35m", s};
 }
 
-inline ColoredText blue(const std::string& s)
+inline ColoredText blue(const std::string &s)
 {
     return {"\x1B[34m", s};
 }
 
-inline ColoredText red(const std::string& s)
+inline ColoredText red(const std::string &s)
 {
     return {"\x1B[31m", s};
 }
 
-inline ColoredText green(const std::string& s)
+inline ColoredText green(const std::string &s)
 {
     return {"\x1B[32m", s};
 }
 
-inline ColoredText yellow(const std::string& s)
+inline ColoredText yellow(const std::string &s)
 {
     return {"\x1B[33m", s};
 }
 
-inline std::string apply_color(const ColoredText& c)
+inline std::string apply_color(const ColoredText &c)
 {
     return std::string(c.color) + c.text + "\x1B[0m";
+}
+
+inline std::string apply_color(const std::string &value)
+{
+    return value;
+}
+
+inline std::string apply_color(const char *value)
+{
+    return std::string(value);
+}
+
+template <typename T>
+inline std::string apply_color(const T &value)
+{
+    return std::to_string(value);
 }
