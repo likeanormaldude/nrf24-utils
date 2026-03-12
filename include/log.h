2118839da369b735243bb7ee4e86db0400dd7a72
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2026-03-12 16:19:55 -0300

    Add multiple args to log functions

--- include/log.h
+++ include/log.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <string>
 #include "log_format.h"
 
@@ -6,40 +7,77 @@ class Log
 {
 public:
     void info(const char* msg);
+    void success(const char* msg);
+    void warn(const char* msg);
+    void error(const char* msg);
+    void debug(const char* msg);
+    void printRaw(const char* msg);
 
-    void replace_placeholder(std::string& msg, const std::string& value)
+    template<typename... Args>
+    void info(const char* fmt, const Args&... args)
+    {
+        info(format_message(fmt, args...).c_str());
+    }
+
+    template<typename... Args>
+    void success(const char* fmt, const Args&... args)
+    {
+        success(format_message(fmt, args...).c_str());
+    }
+
+    template<typename... Args>
+    void warn(const char* fmt, const Args&... args)
+    {
+        warn(format_message(fmt, args...).c_str());
+    }
+
+    template<typename... Args>
+    void error(const char* fmt, const Args&... args)
+    {
+        error(format_message(fmt, args...).c_str());
+    }
+
+    template<typename... Args>
+    void debug(const char* fmt, const Args&... args)
+    {
+        debug(format_message(fmt, args...).c_str());
+    }
+
+    template<typename... Args>
+    void printRaw(const char* fmt, const Args&... args)
+    {
+        printRaw(format_message(fmt, args...).c_str());
+    }
+
+    void testLogColors();
+    std::string timestamp();
+
+private:
+    void print(const char* level, const char* color, const char* msg);
+
+    static void replace_placeholder(std::string& msg, const std::string& value)
     {
         auto pos = msg.find("%t");
         if (pos != std::string::npos)
             msg.replace(pos, 2, value);
     }
 
-    void format_impl(std::string& msg) {}
+    static void format_impl(std::string&) {}
 
     template<typename T, typename... Args>
-    void format_impl(std::string& msg, const T& first, const Args&... rest)
+    static void format_impl(std::string& msg, const T& first, const Args&... rest)
     {
         replace_placeholder(msg, apply_color(first));
         format_impl(msg, rest...);
     }
 
     template<typename... Args>
-    void info(const char* fmt, const Args&... args)
+    static std::string format_message(const char* fmt, const Args&... args)
     {
         std::string msg(fmt);
         format_impl(msg, args...);
-        info(msg.c_str());
+        return msg;
     }
-
-    void success(const char* msg);
-    void warn(const char* msg);
-    void error(const char* msg);
-    void debug(const char* msg);
-    void testLogColors();
-    std::string timestamp();
-
-private:
-    void print(const char* level, const char* color, const char* msg);
 };
 
 extern Log logger;
--- test/test_logger/test_log.cpp
+++ test/test_logger/test_log.cpp
@@ -1,11 +1,19 @@
 #include <unity.h>
-#include "log.h"
 #include <string>
+
+#include "log.h"
 
 #ifdef ARDUINO
 #include <Arduino.h>
 #endif
 
+static void log_test_case(const char* testName, const char* description)
+{
+    logger.printRaw("==================================================");
+    logger.info("Running test: %t", green(testName));
+    logger.printRaw(description);
+    logger.printRaw("==================================================");
+}
 
 void setUp(void)
 {
@@ -15,45 +23,188 @@ void tearDown(void)
 {
 }
 
-
-void test_timestamp_format(void)
+void test_timestamp_should_be_wrapped_in_brackets(void)
 {
-    std::string ts = logger.timestamp();
+    log_test_case(
+        "test_timestamp_should_be_wrapped_in_brackets",
+        "Validates that timestamp output starts with '[' and ends with ']'."
+    );
 
-    TEST_ASSERT_TRUE(!ts.empty());
-    TEST_ASSERT_TRUE(ts.front() == '[');
-    TEST_ASSERT_TRUE(ts.back() == ']');
+    const std::string ts = logger.timestamp();
+
+    TEST_ASSERT_FALSE(ts.empty());
+    TEST_ASSERT_EQUAL('[', ts.front());
+    TEST_ASSERT_EQUAL(']', ts.back());
 }
 
-
-void test_logger_functions(void)
+void test_info_should_accept_plain_message(void)
 {
+    log_test_case(
+        "test_info_should_accept_plain_message",
+        "Validates that logger.info accepts a plain message."
+    );
+
     logger.info("Info test");
-    logger.success("Success test");
-    logger.warn("Warn test");
-    logger.error("Error test");
-    logger.debug("Debug test");
-
-    TEST_ASSERT_TRUE(true);
+    TEST_PASS();
 }
 
-void test_colored_format(void)
+void test_success_should_accept_plain_message(void)
 {
-    logger.info("Colored %t %t", magenta("hello"), blue("world"));
-    TEST_ASSERT_TRUE(true);
+    log_test_case(
+        "test_success_should_accept_plain_message",
+        "Validates that logger.success accepts a plain message."
+    );
+
+    logger.success("Success test");
+    TEST_PASS();
+}
+
+void test_warn_should_accept_plain_message(void)
+{
+    log_test_case(
+        "test_warn_should_accept_plain_message",
+        "Validates that logger.warn accepts a plain message."
+    );
+
+    logger.warn("Warn test");
+    TEST_PASS();
+}
+
+void test_error_should_accept_plain_message(void)
+{
+    log_test_case(
+        "test_error_should_accept_plain_message",
+        "Validates that logger.error accepts a plain message."
+    );
+
+    logger.error("Error test");
+    TEST_PASS();
+}
+
+void test_debug_should_accept_plain_message(void)
+{
+    log_test_case(
+        "test_debug_should_accept_plain_message",
+        "Validates that logger.debug accepts a plain message."
+    );
+
+    logger.debug("Debug test");
+    TEST_PASS();
+}
+
+void test_info_should_accept_formatted_message(void)
+{
+    log_test_case(
+        "test_info_should_accept_formatted_message",
+        "Validates formatted placeholders with logger.info."
+    );
+
+    logger.info("Info %t %t", magenta("hello"), blue("world"));
+    TEST_PASS();
+}
+
+void test_success_should_accept_formatted_message(void)
+{
+    log_test_case(
+        "test_success_should_accept_formatted_message",
+        "Validates formatted placeholders with logger.success."
+    );
+
+    logger.success("Success %t", green("ok"));
+    TEST_PASS();
+}
+
+void test_warn_should_accept_formatted_message(void)
+{
+    log_test_case(
+        "test_warn_should_accept_formatted_message",
+        "Validates formatted placeholders with logger.warn."
+    );
+
+    logger.warn("Warn %t", yellow("careful"));
+    TEST_PASS();
+}
+
+void test_error_should_accept_formatted_message(void)
+{
+    log_test_case(
+        "test_error_should_accept_formatted_message",
+        "Validates formatted placeholders with logger.error."
+    );
+
+    logger.error("Error %t", red("failed"));
+    TEST_PASS();
+}
+
+void test_debug_should_accept_formatted_message(void)
+{
+    log_test_case(
+        "test_debug_should_accept_formatted_message",
+        "Validates formatted placeholders with logger.debug."
+    );
+
+    logger.debug("Debug %t", magenta("trace"));
+    TEST_PASS();
+}
+
+void test_printRaw_should_accept_plain_message(void)
+{
+    log_test_case(
+        "test_printRaw_should_accept_plain_message",
+        "Validates that logger.printRaw accepts a plain message."
+    );
+
+    logger.printRaw("Plain raw message");
+    TEST_PASS();
+}
+
+void test_printRaw_should_accept_formatted_message(void)
+{
+    log_test_case(
+        "test_printRaw_should_accept_formatted_message",
+        "Validates formatted placeholders with logger.printRaw."
+    );
+
+    logger.printRaw("Raw %t %t", magenta("hello"), blue("world"));
+    TEST_PASS();
+}
+
+void test_testLogColors_should_execute_without_failure(void)
+{
+    log_test_case(
+        "test_testLogColors_should_execute_without_failure",
+        "Validates that testLogColors executes fully without failures."
+    );
+
+    logger.testLogColors();
+    TEST_PASS();
 }
 
 int runUnityTests(void)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_timestamp_format);
-    RUN_TEST(test_logger_functions);
-    RUN_TEST(test_colored_format);
+    RUN_TEST(test_timestamp_should_be_wrapped_in_brackets);
+
+    RUN_TEST(test_info_should_accept_plain_message);
+    RUN_TEST(test_success_should_accept_plain_message);
+    RUN_TEST(test_warn_should_accept_plain_message);
+    RUN_TEST(test_error_should_accept_plain_message);
+    RUN_TEST(test_debug_should_accept_plain_message);
+
+    RUN_TEST(test_info_should_accept_formatted_message);
+    RUN_TEST(test_success_should_accept_formatted_message);
+    RUN_TEST(test_warn_should_accept_formatted_message);
+    RUN_TEST(test_error_should_accept_formatted_message);
+    RUN_TEST(test_debug_should_accept_formatted_message);
+
+    RUN_TEST(test_printRaw_should_accept_plain_message);
+    RUN_TEST(test_printRaw_should_accept_formatted_message);
+
+    RUN_TEST(test_testLogColors_should_execute_without_failure);
 
     return UNITY_END();
 }
-
 
 #ifdef ARDUINO
 
@@ -61,7 +212,6 @@ void setup(void)
 {
     Serial.begin(115200);
     delay(2000);
-
     runUnityTests();
 }
 
